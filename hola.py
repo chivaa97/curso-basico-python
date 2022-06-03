961488ff72fb24b764551759343eb76ccc93da6d
Eduardo contreras <chivaa.97@gmail.com>
2022-06-02 22:12:21 -0500

    initial commit

---
+++ hola.py
@@ -0,0 +1,9 @@
+from ast import parse
+
+
+calificacion = input("introduce tu calificacion de examen")
+calificacion = int(calificacion)
+if calificacion>700 : 
+  print("excelente, aprobaste") 
+else :  
+  print("lo siento, no pasaste...")
